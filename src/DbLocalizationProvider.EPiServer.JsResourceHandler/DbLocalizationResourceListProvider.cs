8681fff24256dbbb74dbb6f7157250d630714900
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2018-05-15 22:28:53 +0200

    Fixed removed JsonConvert stuff

--- src/DbLocalizationProvider.EPiServer.JsResourceHandler/DbLocalizationResourceListProvider.cs
+++ src/DbLocalizationProvider.EPiServer.JsResourceHandler/DbLocalizationResourceListProvider.cs
@@ -1,33 +1,21 @@
 ﻿using System.Web;
 using System.Web.Caching;
-using DbLocalizationProvider.Internal;
-using DbLocalizationProvider.Queries;
 using Newtonsoft.Json;
 
 namespace DbLocalizationProvider.EPiServer.JsResourceHandler
 {
     public class DbLocalizationResourceListProvider : IResourceListProvider
     {
-        private readonly ResourceJsonConverter _converter;
-        private readonly ResourceFilter _filter;
+        private readonly DbLocalizationProvider.AspNet.Json.JsonConverter _converter;
 
-        public DbLocalizationResourceListProvider(ResourceFilter filter, ResourceJsonConverter converter)
+        public DbLocalizationResourceListProvider(DbLocalizationProvider.AspNet.Json.JsonConverter converter)
         {
-            _filter = filter;
             _converter = converter;
         }
 
         public string GetJson(string filename, HttpContext context, string languageName, bool debugMode)
         {
-            // for us - filename it's actually root namespace of the resource key to retrieve
-            var resources = new GetAllResources.Query().Execute();
-            var filteredResources = _filter.GetResourcesWithStartingKey(resources, filename);
-
-            // we need to process key names and supported tested classes with "+" symbols in keys -> so we replace those wiyh dots to have proper object nesting on client side
-            filteredResources.ForEach(r => r.ResourceKey = r.ResourceKey.Replace("+", "."));
-
-            return JsonConvert.SerializeObject(_converter.Convert(filteredResources, languageName, ConfigurationContext.Current.EnableInvariantCultureFallback),
-                debugMode ? Formatting.Indented : Formatting.None);
+            return JsonConvert.SerializeObject(_converter.GetJson(filename, languageName), debugMode ? Formatting.Indented : Formatting.None);
         }
 
         public CacheDependency GetCacheDependency()
