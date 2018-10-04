23f243a90b2ec4c2da81f1a3f6497073500aa08c
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2018-10-05 00:40:03 +0300

    weird stuff

--- tests/DbLocalizationProvider.EPiServer.Sample/Models/Pages/SomeWeirdPage.cs
+++ tests/DbLocalizationProvider.EPiServer.Sample/Models/Pages/SomeWeirdPage.cs
@@ -3,13 +3,18 @@ using EPiServer.Core;
 
 namespace DbLocalizationProvider.EPiServer.Sample.Models.Pages
 {
-    [LocalizedResource(Inherited = false)]
+    [LocalizedResource(KeyPrefix = "/contenttypes/aboutuspage/", Inherited = false)]
     public class SomeWeirdPage : PageData
     {
         [Ignore]
-        public ContentArea ContentArea { get; set; }
+        public virtual ContentArea ContentArea { get; set; }
 
         [Include]
         public virtual string SomeProperty { get; set; }
+
+        [ResourceKey("properties/herotitle/caption", Value = "Hero Title")]
+        [ResourceKey("properties/herotitle/help", Value =
+            "Set hero title, if nothings is set default one will be used.")]
+        public virtual string HeroTitle { get; set; }
     }
 }
