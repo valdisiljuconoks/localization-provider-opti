e1faf14ea5dc5143dbe24511e54749c4c28a5b37
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2018-10-05 00:32:17 +0300

    added weird page

---
+++ tests/DbLocalizationProvider.EPiServer.Sample/Models/Pages/SomeWeirdPage.cs
@@ -0,0 +1,15 @@
+﻿using DbLocalizationProvider.Abstractions;
+using EPiServer.Core;
+
+namespace DbLocalizationProvider.EPiServer.Sample.Models.Pages
+{
+    [LocalizedResource(Inherited = false)]
+    public class SomeWeirdPage : PageData
+    {
+        [Ignore]
+        public ContentArea ContentArea { get; set; }
+
+        [Include]
+        public virtual string SomeProperty { get; set; }
+    }
+}
