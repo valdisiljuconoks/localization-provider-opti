d437b1c0faa2749217838a7c1f05dd70f4739432
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-01-05 20:44:19 +0200

    Set version to 6.0

--- src/DbLocalizationProvider.AdminUI.EPiServer/Properties/AssemblyInfo.cs
+++ src/DbLocalizationProvider.AdminUI.EPiServer/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DbLocalizationProvider.AdminUI.EPiServer")]
 [assembly: AssemblyDescription("DbLocalizationProvider management user interface for EPiServer")]
 [assembly: Guid("aa5fcd04-8113-4f0b-8e8e-5cf365b3d245")]
-[assembly: AssemblyVersion("5.8.0.0")]
-[assembly: AssemblyFileVersion("5.8.0.0")]
-[assembly: AssemblyInformationalVersion("5.8.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0")]
--- src/DbLocalizationProvider.EPiServer/Properties/AssemblyInfo.cs
+++ src/DbLocalizationProvider.EPiServer/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DbLocalizationProvider.EPiServer")]
 [assembly: AssemblyDescription("Database driven LocalizationProvider for EPiServer")]
 [assembly: Guid("86cbfec7-8763-4bba-b760-0a0c977c8c36")]
-[assembly: AssemblyVersion("5.8.0.0")]
-[assembly: AssemblyFileVersion("5.8.0.0")]
-[assembly: AssemblyInformationalVersion("5.8.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyInformationalVersion("6.0.0")]
