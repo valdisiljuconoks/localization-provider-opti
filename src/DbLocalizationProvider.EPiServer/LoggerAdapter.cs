5c5533fbe002b636cb32736ceec3dcdc2aa0a8ba
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-03-01 20:23:50 +0200

    Fixed license header

--- src/DbLocalizationProvider.EPiServer/LoggerAdapter.cs
+++ src/DbLocalizationProvider.EPiServer/LoggerAdapter.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Valdis Iljuconoks. All rights reserved.
-// Licensed under MIT. See the LICENSE file in the project root for more information
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
 
 using System;
 using EPiServer.Logging;
