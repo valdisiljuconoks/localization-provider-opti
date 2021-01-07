616b00c8ce2108ad07e44d27b4fd0bb18f9c920c
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2021-01-07 18:25:03 +0200

    Set version to 6.4.1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Valdis Iljuconoks, https://tech-fellow.net")]
 [assembly: AssemblyProduct("DbLocalizationProvider")]
-[assembly: AssemblyCopyright("Tech Fellow Consulting, Copyright © 2020")]
+[assembly: AssemblyCopyright("Tech Fellow Consulting, Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
