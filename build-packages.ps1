ec83f7e94db55acf9896aa673c3a67c8422b7e83
valdis iljuconoks <valdis.iljuconoks@outlook.com>
2021-10-01 15:03:52 +0300

    moved content types to any/any

--- build-packages.ps1
+++ build-packages.ps1
@@ -1,5 +1,3 @@
-Add-Type -assembly "system.io.compression.filesystem"
-
 cd .\.nuget
 
 # episerver libraries
@@ -7,7 +5,6 @@ cd .\.nuget
 cd .\..\src\DbLocalizationProvider.EPiServer\
 dotnet build -c Release
 dotnet pack -c Release
-# dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg
 copy .\bin\Release\DbLocalizationProvider.EPiServer.*.nupkg .\..\..\.nuget\
 copy .\bin\Release\DbLocalizationProvider.EPiServer.*.snupkg .\..\..\.nuget\
 cd .\..\..\.nuget\
@@ -15,7 +12,6 @@ cd .\..\..\.nuget\
 cd .\..\src\DbLocalizationProvider.AdminUI.EPiServer\
 dotnet build -c Release
 dotnet pack -c Release
-# dotnet pack --include-symbols -p:SymbolPackageFormat=snupkg
 copy .\bin\Release\DbLocalizationProvider.AdminUI.EPiServer.*.nupkg .\..\..\.nuget\
 copy .\bin\Release\DbLocalizationProvider.AdminUI.EPiServer.*.snupkg .\..\..\.nuget\
 cd .\..\..\.nuget\
