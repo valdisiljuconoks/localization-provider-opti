38ac6a0a7c9f89e2242715d30794a6364f1e8f7f
valdis <valdis.iljuconoks@outlook.com>
2022-11-07 08:08:17 +0200

    set version to 7.5

--- push-packages.ps1
+++ push-packages.ps1
@@ -1,9 +1,9 @@
 cd .\.nuget
 
-nuget push .\DbLocalizationProvider.EPiServer.7.4.0.nupkg -source https://nuget.episerver.com/feed/packages.svc
-nuget push .\DbLocalizationProvider.EPiServer.JsResourceHandler.7.4.0.nupkg -source https://nuget.episerver.com/feed/packages.svc
-nuget push .\DbLocalizationProvider.AdminUI.EPiServer.7.4.0.nupkg -source https://nuget.episerver.com/feed/packages.svc/
-nuget push .\DbLocalizationProvider.AdminUI.EPiServer.Xliff.7.4.0.nupkg -source https://nuget.episerver.com/feed/packages.svc/
-nuget push .\DbLocalizationProvider.MigrationTool.7.4.0.nupkg -source https://nuget.episerver.com/feed/packages.svc/
+nuget push .\DbLocalizationProvider.EPiServer.7.5.0.nupkg -source https://nuget.episerver.com/feed/packages.svc
+nuget push .\DbLocalizationProvider.EPiServer.JsResourceHandler.7.5.0.nupkg -source https://nuget.episerver.com/feed/packages.svc
+nuget push .\DbLocalizationProvider.AdminUI.EPiServer.7.5.0.nupkg -source https://nuget.episerver.com/feed/packages.svc/
+nuget push .\DbLocalizationProvider.AdminUI.EPiServer.Xliff.7.5.0.nupkg -source https://nuget.episerver.com/feed/packages.svc/
+nuget push .\DbLocalizationProvider.MigrationTool.7.5.0.nupkg -source https://nuget.episerver.com/feed/packages.svc/
 
 cd ..\
